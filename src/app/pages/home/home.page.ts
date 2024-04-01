05a6db7a0c441094915cf1768ce646c2d7bff0b1
César Rafael <cesarrdev@gmail.com>
2024-04-01 07:28:21 -0300

    feat: Chat modal assistente virtual

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,26 @@
 import { FormsModule } from '@angular/forms';
-import { Component, viewChild } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonItem, IonLabel, IonInput, IonButton, IonImg, IonIcon, IonText, IonSelect, IonSelectOption, IonModal, IonButtons } from '@ionic/angular/standalone';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonImg,
+  IonIcon,
+  IonText,
+  IonSelect,
+  IonSelectOption,
+  IonModal,
+  IonButtons,
+  IonList,
+} from '@ionic/angular/standalone';
 import { TableModule } from 'primeng/table';
 import { OverlayEventDetail } from '@ionic/core/components';
 
@@ -9,22 +29,44 @@ import { OverlayEventDetail } from '@ionic/core/components';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [FormsModule, TableModule, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonItem, IonLabel, IonInput, IonButton, IonImg, IonIcon, IonText, IonSelect, IonSelectOption, IonModal, IonButtons]
-
+  imports: [
+    IonList,
+    FormsModule,
+    TableModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonImg,
+    IonIcon,
+    IonText,
+    IonSelect,
+    IonSelectOption,
+    IonModal,
+    IonButtons,
+  ],
 })
 export class HomePage {
-  // @viewChild(IonModal) modal: IonModal | undefined;
+  @ViewChild(IonModal) modal: IonModal | undefined;
 
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  question: string = '';
-  years: Array<number> = []
+  chatMessages: { name: string; message: string }[] = [];
+
+  question: { name: string; message: string } = { name: 'User', message: '' };
+  years: Array<number> = [];
   currentYear: number = new Date().getFullYear();
-  panelButtons: Array<{title: string, icon: string}> = [
-    {title: 'ADICIONAR', icon: 'ic--baseline-plus.svg'},
-    {title: 'BUSCAR', icon: 'search-sharp.svg'},
-    {title: 'FILTRAR', icon: 'mdi--filter.svg'},
-    {title: 'AÇÕES', icon: 'mdi--gear.svg'},
-  ]
+  panelButtons: Array<{ title: string; icon: string }> = [
+    { title: 'ADICIONAR', icon: 'ic--baseline-plus.svg' },
+    { title: 'BUSCAR', icon: 'search-sharp.svg' },
+    { title: 'FILTRAR', icon: 'mdi--filter.svg' },
+    { title: 'AÇÕES', icon: 'mdi--gear.svg' },
+  ];
   table: any = {
     cols: [
       { field: '', header: '' },
@@ -39,39 +81,174 @@ export class HomePage {
       { field: 'Setembro', header: 'Setembro' },
       { field: 'Outubro', header: 'Outubro' },
       { field: 'Novembro', header: 'Novembro' },
-      { field: 'Dezembro', header: 'Dezembro' }
+      { field: 'Dezembro', header: 'Dezembro' },
     ],
     rows: [
-     ['Receita', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Despesa', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Demonstrativo de Resultado', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Variação do Exercício', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Ativo', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Passivo', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Patrimônio Líquido', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Variação', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-     ['Auditoria Patrimonial', 1000, 2000, 3000, 4000, 5000, 5000, 5000, 5000, 5000, 5000, 400, 600],
-    ]
-  }
+      [
+        'Receita',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Despesa',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Demonstrativo de Resultado',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Variação do Exercício',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Ativo',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Passivo',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Patrimônio Líquido',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Variação',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+      [
+        'Auditoria Patrimonial',
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        5000,
+        400,
+        600,
+      ],
+    ],
+  };
 
   constructor() {
-    for(let i = this.currentYear - 6; i <= this.currentYear + 6; i++) {
+    for (let i = this.currentYear - 6; i <= this.currentYear + 6; i++) {
       this.years.push(i);
     }
   }
 
-  // cancel() {
-  //   this.modal?.dismiss(null, 'cancel');
-  // }
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+  }
 
   // confirm() {
-  //   this.modal?.dismiss(this.question, 'confirm');
+  //   // this.modal?.dismiss(this.question, 'confirm');
   // }
+
+  sendMessage() {
+    const newMessage = { ...this.question };
+
+    if (newMessage.message.trim() !== '') {
+      this.chatMessages.push(newMessage);
+      this.question.message = '';
+    }
+  }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
+      this.chatMessages[0].message = `Hello, ${ev.detail.data}!`;
     }
   }
 }
